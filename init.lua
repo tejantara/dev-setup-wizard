d9459d0fe0dd557a49e15006bbd3e88fe94fd196
Ivan Tejantara <ivan@tejantara.com>
2024-12-16 20:35:58 +0700

    Create init.lua

---
+++ init.lua
@@ -0,0 +1,261 @@
+-- Global variable
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+vim.g.coc_global_extensions = { 'coc-json', 'coc-css', 'coc-tsserver', 'coc-prettier' }
+
+-- Install package manager
+--    https://github.com/folke/lazy.nvim
+--    `:help lazy.nvim.txt` for more info
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system {
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable', -- latest stable release
+    lazypath,
+  }
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- NOTE: Here is where you install your plugins.
+--  You can configure plugins using the `config` key.
+--
+--  You can also configure plugins after the setup call,
+--    as they will be available in your neovim runtime.
+require('lazy').setup({
+  -- NOTE: First, some plugins that don't require any configuration
+
+  -- Git related plugins
+  'tpope/vim-fugitive',
+  'tpope/vim-rhubarb',
+
+  -- Detect tabstop and shiftwidth automatically
+  'tpope/vim-sleuth',
+  -- Useful plugin to show you pending keybinds.
+  -- { 'folke/which-key.nvim',          opts = {} },
+  {
+    -- Adds git releated signs to the gutter, as well as utilities for managing changes
+    'lewis6991/gitsigns.nvim',
+    opts = {
+      -- See `:help gitsigns.txt`
+      signs = {
+        add = { text = '+' },
+        change = { text = '~' },
+        delete = { text = '_' },
+        topdelete = { text = '‾' },
+        changedelete = { text = '~' },
+      },
+    },
+  },
+  {
+    'ellisonleao/gruvbox.nvim',
+    config = function()
+      vim.cmd('colorscheme gruvbox')
+    end
+  },
+  {
+    -- Set lualine as statusline
+    'nvim-lualine/lualine.nvim',
+    -- See `:help lualine.txt`
+    opts = {
+      options = {
+        icons_enabled = false,
+        theme = 'gruvbox-material',
+        component_separators = '|',
+        section_separators = '',
+      },
+    },
+  },
+  {
+    -- Add indentation guides even on blank lines
+    'lukas-reineke/indent-blankline.nvim',
+    -- Enable `lukas-reineke/indent-blankline.nvim`
+    -- See `:help indent_blankline.txt`
+    opts = {
+      char = '┊',
+      show_trailing_blankline_indent = false,
+    },
+  },
+
+  -- "gc" to comment visual regions/lines
+  { 'numToStr/Comment.nvim',         opts = {} },
+
+  {
+    -- Highlight, edit, and navigate code
+    'nvim-treesitter/nvim-treesitter',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter-textobjects',
+    },
+    config = function()
+      pcall(require('nvim-treesitter.install').update { with_sync = true })
+    end,
+  },
+  {
+    'neoclide/coc.nvim',
+    branch = 'release',
+  },
+  'junegunn/fzf',
+  'junegunn/fzf.vim',
+}, {})
+
+-- [[ Setting options ]]
+-- See `:help vim.o`
+
+-- Set file format
+vim.o.ff = 'unix'
+
+-- Set highlight on search
+vim.o.hlsearch = false
+
+-- Make line numbers default
+vim.wo.number = true
+
+-- Disable mouse mode
+vim.o.mouse = ''
+
+-- Sync clipboard between OS and Neovim.
+--  Remove this option if you want your OS clipboard to remain independent.
+--  See `:help 'clipboard'`
+vim.o.clipboard = 'unnamedplus'
+
+-- Enable break indent
+vim.o.breakindent = true
+
+-- file extension / directory to be ignored
+vim.o.wildignore = '*\\tmp\\*, *.swp,*.zip,*.exe,*\\node_modules\\*,*\\dist\\*'
+
+-- Save undo history
+vim.o.undofile = true
+
+-- Case insensitive searching UNLESS /C or capital in search
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+-- Keep signcolumn on by default
+vim.wo.signcolumn = 'yes'
+
+-- Decrease update time
+vim.o.updatetime = 250
+vim.o.timeout = true
+vim.o.timeoutlen = 300
+
+-- Set completeopt to have a better completion experience
+-- vim.o.completeopt = 'menuone,noselect'
+
+-- NOTE: You should make sure your terminal supports this
+vim.o.termguicolors = true
+
+-- [[ Basic Keymaps ]]
+
+-- Diagnostic
+vim.api.nvim_set_keymap('n', ']d', ':call CocAction("diagnosticNext")<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '[d', ':call CocAction("diagnosticPrevious")<CR>', { noremap = true, silent = true })
+
+-- Organize Import
+vim.api.nvim_create_user_command('O', 'CocCommand editor.action.organizeImport', {})
+
+-- Prettier format file
+vim.api.nvim_create_user_command('F', 'CocCommand prettier.formatFile', {})
+
+-- go to definition
+vim.api.nvim_set_keymap('n', 'gd', ':CocCommand tsserver.goToSourceDefinition<CR>', { noremap = true, silent = true })
+
+-- go to references
+vim.api.nvim_set_keymap('n', 'gi', ':CocCommand tsserver.findAllFileReferences<CR>', { noremap = true, silent = true })
+
+-- find file
+vim.api.nvim_set_keymap('n', '<C-p>', ':FZF<CR>', { noremap = true, silent = true })
+
+-- ripgrep
+vim.api.nvim_set_keymap('n', '<C-t>', ':Rg<CR>', { noremap = true, silent = true })
+
+-- [[ Highlight on yank ]]
+-- See `:help vim.highlight.on_yank()`
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  pattern = '*',
+})
+
+-- [[ gruvbox theme ]]
+-- need to call 'colorscheme' again to overwrite indent line color
+require('gruvbox').setup({
+  italic = {
+    strings = false,
+    comments = false,
+    folds = false,
+  },
+})
+vim.cmd('colorscheme gruvbox');
+
+-- [[ Configure Treesitter ]]
+-- See `:help nvim-treesitter`
+require('nvim-treesitter.configs').setup {
+  -- Add languages to be installed here that you want installed for treesitter
+  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'help', 'vim' },
+
+  -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
+  auto_install = false,
+
+  highlight = { enable = true },
+  indent = { enable = true, disable = { 'python' } },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = '<c-space>',
+      node_incremental = '<c-space>',
+      scope_incremental = '<c-s>',
+      node_decremental = '<M-space>',
+    },
+  },
+  textobjects = {
+    select = {
+      enable = true,
+      lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+      keymaps = {
+        -- You can use the capture groups defined in textobjects.scm
+        ['aa'] = '@parameter.outer',
+        ['ia'] = '@parameter.inner',
+        ['af'] = '@function.outer',
+        ['if'] = '@function.inner',
+        ['ac'] = '@class.outer',
+        ['ic'] = '@class.inner',
+      },
+    },
+    move = {
+      enable = true,
+      set_jumps = true, -- whether to set jumps in the jumplist
+      goto_next_start = {
+        [']]'] = '@function.outer',
+        [']m'] = '@class.outer',
+      },
+      goto_next_end = {
+        [']['] = '@function.outer',
+        [']M'] = '@class.outer',
+      },
+      goto_previous_start = {
+        ['[['] = '@function.outer',
+        ['[m'] = '@class.outer',
+      },
+      goto_previous_end = {
+        ['[]'] = '@function.outer',
+        ['[M'] = '@class.outer',
+      },
+    },
+    swap = {
+      enable = true,
+      swap_next = {
+        ['<leader>a'] = '@parameter.inner',
+      },
+      swap_previous = {
+        ['<leader>A'] = '@parameter.inner',
+      },
+    },
+  },
+}
+
